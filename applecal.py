79af69adc28167c33a9498fc38b63be1b7720056
prageethpasyala <prageethpasyala@gmail.com>
2021-11-11 01:24:53 +0000

    metric convertion

--- applecal.py
+++ applecal.py
@@ -1,15 +1,20 @@
 
 print("\n")
-print("Apple weight calculater")
-print("------------------------")
-apple = input("Enter how many apples : ")
-weightunit = input("you want kg or lbs? : K or L : ")
-if weightunit == "K":
+print("Apple scale")
+print("===========")
+
+apple = input("Enter how many apples you bought: ")
+weightunit = input("Select the metric : type K (kg) or L (lbs) : ")
+if weightunit == "K" or weightunit =="k":
     applekg = float(apple)/5
-    print("Weight of your apple is : " + str(applekg) + "Kg")
     print("\n")
-elif weightunit == "L":
+    print("Weight of your " + str(apple) + " apples is : " + str(applekg) + " Kg")
+    print("\n")
+elif weightunit == "L" or weightunit=="l":
     applekg = (float(apple)/5) * 2.2
-    print("Weight of your apple is : " + str(applekg) + "Lbs")
     print("\n")
+    print("Weight of your " + str(apple) + " apples is : " + str(applekg) + " Lbs")
+    print("\n")
+
+    
 
